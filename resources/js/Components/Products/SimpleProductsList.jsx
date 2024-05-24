64dc4262f6600142cb0e30bafe1c64539df12ac8
deluxesande <deluxesande@gmail.com>
2024-05-24 21:19:26 +0300

    (Fix): home screen padding

--- resources/js/Components/Products/SimpleProductsList.jsx
+++ resources/js/Components/Products/SimpleProductsList.jsx
@@ -3,8 +3,14 @@ import Text from "@/Components/Text";
 
 export default function SimpleProductsList({ user }) {
     return (
-        <div className="w-full px-4 py-6 sm:px-6 sm:py-12 lg:px-8">
-            <h2 className="sr-only">Products</h2>
+        <div className="w-full px-4 py-6 sm:px-6 sm:py-12 lg:mb-16 lg:py-16 lg:px-8">
+            <span className="sr-only">Products</span>
+            <Text
+                variant="h2"
+                className="text-2xl font-bold tracking-tight mb-8"
+            >
+                Products
+            </Text>
 
             <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                 {products.map((product) => (
--- resources/js/utils/Lists.js
+++ resources/js/utils/Lists.js
@@ -33,6 +33,176 @@ const products = [
         ],
     },
 
+    {
+        id: 2,
+        name: "Basic Tee 6-Pack",
+        price: "$192",
+        rating: 3.9,
+        reviewCount: 117,
+        href: "#",
+        imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/product-quick-preview-02-detail.jpg",
+        imageAlt:
+            "Two each of gray, white, and black shirts arranged on table.",
+        colors: [
+            {
+                name: "white",
+            },
+            {
+                name: "gray",
+            },
+            {
+                name: "black",
+            },
+        ],
+        sizes: [
+            { name: "XXS", inStock: true },
+            { name: "XS", inStock: true },
+            { name: "S", inStock: true },
+            { name: "M", inStock: true },
+            { name: "L", inStock: true },
+            { name: "XL", inStock: true },
+            { name: "XXL", inStock: true },
+            { name: "XXXL", inStock: false },
+        ],
+    },
+
+    {
+        id: 3,
+        name: "Basic Tee 6-Pack",
+        price: "$192",
+        rating: 3.9,
+        reviewCount: 117,
+        href: "#",
+        imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/product-quick-preview-02-detail.jpg",
+        imageAlt:
+            "Two each of gray, white, and black shirts arranged on table.",
+        colors: [
+            {
+                name: "white",
+            },
+            {
+                name: "gray",
+            },
+            {
+                name: "black",
+            },
+        ],
+        sizes: [
+            { name: "XXS", inStock: true },
+            { name: "XS", inStock: true },
+            { name: "S", inStock: true },
+            { name: "M", inStock: true },
+            { name: "L", inStock: true },
+            { name: "XL", inStock: true },
+            { name: "XXL", inStock: true },
+            { name: "XXXL", inStock: false },
+        ],
+    },
+
+    {
+        id: 4,
+        name: "Basic Tee 6-Pack",
+        price: "$192",
+        rating: 3.9,
+        reviewCount: 117,
+        href: "#",
+        imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/product-quick-preview-02-detail.jpg",
+        imageAlt:
+            "Two each of gray, white, and black shirts arranged on table.",
+        colors: [
+            {
+                name: "white",
+            },
+            {
+                name: "gray",
+            },
+            {
+                name: "black",
+            },
+        ],
+        sizes: [
+            { name: "XXS", inStock: true },
+            { name: "XS", inStock: true },
+            { name: "S", inStock: true },
+            { name: "M", inStock: true },
+            { name: "L", inStock: true },
+            { name: "XL", inStock: true },
+            { name: "XXL", inStock: true },
+            { name: "XXXL", inStock: false },
+        ],
+    },
+
+    {
+        id: 5,
+        name: "Basic Tee 6-Pack",
+        price: "$192",
+        rating: 3.9,
+        reviewCount: 117,
+        href: "#",
+        imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/product-quick-preview-02-detail.jpg",
+        imageAlt:
+            "Two each of gray, white, and black shirts arranged on table.",
+        colors: [
+            {
+                name: "white",
+            },
+            {
+                name: "gray",
+            },
+            {
+                name: "black",
+            },
+        ],
+        sizes: [
+            { name: "XXS", inStock: true },
+            { name: "XS", inStock: true },
+            { name: "S", inStock: true },
+            { name: "M", inStock: true },
+            { name: "L", inStock: true },
+            { name: "XL", inStock: true },
+            { name: "XXL", inStock: true },
+            { name: "XXXL", inStock: false },
+        ],
+    },
+
+    {
+        id: 6,
+        name: "Basic Tee 6-Pack",
+        price: "$192",
+        rating: 3.9,
+        reviewCount: 117,
+        href: "#",
+        imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/product-quick-preview-02-detail.jpg",
+        imageAlt:
+            "Two each of gray, white, and black shirts arranged on table.",
+        colors: [
+            {
+                name: "white",
+            },
+            {
+                name: "gray",
+            },
+            {
+                name: "black",
+            },
+        ],
+        sizes: [
+            { name: "XXS", inStock: true },
+            { name: "XS", inStock: true },
+            { name: "S", inStock: true },
+            { name: "M", inStock: true },
+            { name: "L", inStock: true },
+            { name: "XL", inStock: true },
+            { name: "XXL", inStock: true },
+            { name: "XXXL", inStock: false },
+        ],
+    },
+
     // More products...
 ];
 
